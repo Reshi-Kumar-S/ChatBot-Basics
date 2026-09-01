bd13040db760a65dcd2af5f49e8e9fc44e2d94d6
Reshi-kumar-s <rishikumarsk18@gmail.com>
2026-09-01 23:25:04 +0530

    nlp code update for beginer use

--- Intent-Based Chatbot Using JSON/spacy_chat.py
+++ Intent-Based Chatbot Using JSON/spacy_chat.py
@@ -4,7 +4,7 @@ import spacy
 
 # Load spaCy model
 nlp = spacy.load("en_core_web_sm")
-
+#python -m spacy download en_core_web_sm
 # Load intents
 with open("responses.json", "r", encoding="utf-8") as file:
     data = json.load(file)
